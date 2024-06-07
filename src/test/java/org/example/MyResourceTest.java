c4585fe0f6a903ef4b1f1460b883706765a7495f
diego <diego.scavalcanti@gmail.com>
2024-06-07 00:27:45 -0300

    versao do jersey atualizada

--- src/test/java/org/example/MyResourceTest.java
+++ src/test/java/org/example/MyResourceTest.java
@@ -1,14 +1,16 @@
 package org.example;
 
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.WebTarget;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyResourceTest {
